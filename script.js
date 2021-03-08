77133020f77060fbe59d489b6b540668b0c4122e
Lesia Hrynko <melaverde1987@gmail.com>
2021-03-08 11:52:26 +0100

    basic done

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-var max = Math.floor(Math.random() * 26);
-var min = Math.floor(Math.random() * -6);
-var mod = 10;
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-var weather = 15;
+var max = Math.floor(Math.random() * 41);
+var min = Math.floor(Math.random() * -41);
 
-if (weather > min && weather < mod) {
+var weather = getRndInteger(min, max)
+
+console.log(weather);
+
+
+var coldMin = -5;
+var coldMax = 10;
+var moderateMin = 11;
+var moderateMax = 31;
+var hotMin = 32;
+
+if (weather >= coldMin && weather <= coldMax) {
     document.getElementById("box").innerHTML = 'The weather is cold';
-} else if  (weather > mod && weather < max) {
+} else if (weather >= moderateMin && weather <= moderateMax) {
     document.getElementById("box").innerHTML = 'The weather is moderate';
+} else if (weather >= hotMin ) {
+    document.getElementById("box").innerHTML = 'The weather is hot';
 } else {
     document.getElementById("box").innerHTML = 'The weather is unknown';
+};
+
+
+var array = [1, 7, -3, 9];
+let largest = 0;
+
+
+for (i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+        largest = array[i];
+    }
 }
+document.getElementById("box2").innerHTML = largest;
+
+
